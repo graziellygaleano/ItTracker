4e3eb6a114c2541c81096282474896577ec854cd
Grazielly Galeano <graziellygaleano@gmail.com>
2026-06-24 21:24:14 -0300

    commitando oq não foi antes por algum razão

--- js/gerenciaLogin.js
+++ js/gerenciaLogin.js
@@ -1,34 +1,15 @@
-
-let userConectado = JSON.parse(localStorage.getItem('userConectado')) || false;
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    const botaoPerfil = document.querySelector(".nav-link-perfil") || document.querySelector("a[href='perfil.html']");
-
-    if (botaoPerfil) {
-        botaoPerfil.addEventListener("click", (event) => {
-            event.preventDefault(); 
-
-
-            if (userConectado === false) {
-                window.location.href = "login.html";
-            } else {
-                window.location.href = "perfil.html";
-            }
-        });
-    }
-});
+export function obterStatusLogin() {
+    return JSON.parse(localStorage.getItem('userConectado')) || false;
+}
 
 export function logar() {
-    userConectado = true;
     console.log('Conectou user');
-    localStorage.setItem('userConectado', JSON.stringify(userConectado));
+    localStorage.setItem('userConectado', JSON.stringify(true));
 }
 
 export function deslogar() {
-    userConectado = false;
     console.log('Desconectou user');
-    localStorage.setItem('userConectado', JSON.stringify(userConectado));
-    alert("Usuário desconectado com sucesso!")
-    
+    localStorage.setItem('userConectado', JSON.stringify(false));
+    alert("Usuário desconectado com sucesso!");
+    window.location.replace("index.html");
 }
--- js/index.js
+++ js/index.js
@@ -1,15 +1,46 @@
+import { obterStatusLogin, deslogar } from "./gerenciaLogin.js";
+
 document.addEventListener("DOMContentLoaded", () => {
 
-    const botaLogin = document.querySelector(".nav-link-perfil") || document.querySelector("a[href='perfil.html']");
+    const estaLogado = obterStatusLogin();
+    
+    console.log("Status do usuário atualizado na Home:", estaLogado);
 
+    const botaoLogin = document.getElementById('botaoLogin');
+    if (botaoLogin) {
+        botaoLogin.addEventListener("click", (event) => {
+            event.preventDefault(); 
+            if (estaLogado === false) {
+                window.location.href = "login.html";
+            } else {
+                window.location.href = "perfil.html";
+            }
+        });
+    }
+
+
+    const botaoCadastro = document.getElementById('botaoCadastro');
+    if (botaoCadastro) {
+        botaoCadastro.addEventListener("click", (event) => {
+            event.preventDefault(); 
+            if (estaLogado === false) {
+                window.location.href = "cadastro.html"; 
+            } else {
+                deslogar()
+                window.location.href = "cadastro.html";   
+            }
+        });
+    }
+
+
+    const botaoPerfil = document.querySelector(".img-perfil")?.parentElement || document.querySelector("a[href='perfil.html']");
     if (botaoPerfil) {
         botaoPerfil.addEventListener("click", (event) => {
             event.preventDefault(); 
-
-
-            if (userConectado === false) {
+            if (estaLogado === false) {
                 window.location.href = "login.html";
             } else {
+
                 window.location.href = "perfil.html";
             }
         });
